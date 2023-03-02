2d6bde3c89309d52f97678cb4ec082eda76879bb
Latifa forozan <la.forozan@gmail.com>
2023-03-02 10:24:46 -0800

    Selenium Grid with Jenkins

--- src/test/java/cydeo/CukesRunner.java
+++ src/test/java/cydeo/CukesRunner.java
@@ -10,8 +10,11 @@ import org.junit.runner.RunWith;
                 "json:target/cucumber.json",
                 "html:target/cucumber/report.html",
                 "junit:target/junit/junit-report.xml",
-                "rerun:target/rerun.txt",},
+                "rerun:target/rerun.txt",
+        },
+
         features = "src/test/resources/features",
         publish = true
+
 )
 public class CukesRunner {}
